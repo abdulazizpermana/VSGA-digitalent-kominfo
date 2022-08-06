895a6d5fed92ab0dc4e80d491af94a0ccfbc66c0
abdul <abdulazizpermana106@gmail.com>
2022-08-06 13:45:48 +0700

    added linear layout

--- app/src/main/java/com/example/vsga_digitalent_kominfo/MainActivity.java
+++ app/src/main/java/com/example/vsga_digitalent_kominfo/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.vsga_digitalent_kominfo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    button toastButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toastButton = findViewById(R.id.toast);
+        toastButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        "Button is clicked", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
