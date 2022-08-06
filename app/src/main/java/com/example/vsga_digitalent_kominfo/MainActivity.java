669e4607aabd6206f1bc6be58e2cd509953821e9
abdul <abdulazizpermana106@gmail.com>
2022-08-06 13:12:48 +0700

    first commit

---
+++ app/src/main/java/com/example/vsga_digitalent_kominfo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.vsga_digitalent_kominfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
